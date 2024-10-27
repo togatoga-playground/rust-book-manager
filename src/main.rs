776aea294a6b15b38e4ebd9279c12599c853c05e
togatoga <hitoshitogasaki@gmail.com>
2024-10-27 16:34:55 +0900

    Fix typo

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,40 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
 use anyhow::Result;
-use axum::http::StatusCode;
+use axum::{extract::State, http::StatusCode};
+use sqlx::PgPool;
 
 pub async fn health_check() -> StatusCode {
     StatusCode::OK
 }
 
+async fn health_check_db(State(db): State<PgPool>) -> StatusCode {
+    match sqlx::query("SELECT 1").fetch_one(&db).await {
+        Ok(_) => StatusCode::OK,
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    let app = axum::Router::new().route("/health", axum::routing::get(health_check));
+    let database_cfg = DatabaseConfig {
+        host: "localhost".to_string(),
+        port: 5432,
+        username: "app".to_string(),
+        password: "passwd".to_string(),
+        database: "app".to_string(),
+    };
+    let conn_pool = connect_database_with(database_cfg);
+
+    let app = axum::Router::new()
+        .route(
+            "/health",
+            axum::routing::get(health_check),
+        )
+        .route(
+            "/health/db",
+            axum::routing::get(health_check_db).with_state(conn_pool),
+        );
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
     let listener = tokio::net::TcpListener::bind(addr).await?;
     println!("Listening on: {}", addr);
@@ -17,8 +42,37 @@ async fn main() -> Result<()> {
     Ok(axum::serve(listener, app).await?)
 }
 
+struct DatabaseConfig {
+    pub host: String,
+    pub port: u16,
+    pub username: String,
+    pub password: String,
+    pub database: String,
+}
+
+impl From<DatabaseConfig> for sqlx::postgres::PgConnectOptions {
+    fn from(config: DatabaseConfig) -> Self {
+        sqlx::postgres::PgConnectOptions::new()
+            .host(&config.host)
+            .port(config.port)
+            .username(&config.username)
+            .password(&config.password)
+            .database(&config.database)
+    }
+}
+
+fn connect_database_with(cfg: DatabaseConfig) -> PgPool {
+    PgPool::connect_lazy_with(cfg.into())
+}
+
 #[tokio::test]
 async fn health_check_works() {
     let status_code = health_check().await;
+    assert_eq!(status_code, StatusCode::OK);
+}
+
+#[sqlx::test]
+async fn health_check_db_works(pool: PgPool) {
+    let status_code = health_check_db(State(pool)).await;
     assert_eq!(status_code, StatusCode::OK);
 }
