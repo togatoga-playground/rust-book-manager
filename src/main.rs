9eb611e49fd89baac461d2b39bc44e50000fe524
togatoga <hitoshitogasaki@gmail.com>
2024-10-27 16:02:07 +0900

    Impl health

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{Ipv4Addr, SocketAddr};
+
+use anyhow::Result;
+use axum::http::StatusCode;
+
+pub async fn health_check() -> StatusCode {
+    StatusCode::OK
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app = axum::Router::new().route("/health", axum::routing::get(health_check));
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
+    let listener = tokio::net::TcpListener::bind(addr).await?;
+    println!("Listening on: {}", addr);
+
+    Ok(axum::serve(listener, app).await?)
+}
+
+#[tokio::test]
+async fn health_check_works() {
+    let status_code = health_check().await;
+    assert_eq!(status_code, StatusCode::OK);
 }
