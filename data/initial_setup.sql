f3d1d3b2c942ffc309bbf8c4f19b8a5f564d6103
togatoga <hitoshitogasaki@gmail.com>
2024-11-01 11:45:11 +0900

    Update initial_setup.sql

---
+++ data/initial_setup.sql
@@ -0,0 +1,18 @@
+INSERT INTO
+    roles (name)
+VALUES
+    ('Admin'),
+    ('User');
+ON CONFLICT DO NOTHING;
+
+INSERT INTO
+    users (name, email, password_hash, role_id)
+SELECT
+    'Eleazar Fig'
+    'eleazar.fig@example.com',
+    '$2b$12$QPYXNL9a4VsBBw4j877gVOt5g1wfjBzA0xrDkDbJQScgQhQk31o76',
+    role_id
+FROM
+    roles
+WHERE
+    name LIKE 'Admin';
