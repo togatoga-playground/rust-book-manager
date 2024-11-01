e557a374c3422a32880edeecf4b89a58297115b2
togatoga <hitoshitogasaki@gmail.com>
2024-11-01 11:50:14 +0900

    Update

--- data/initial_setup.sql
+++ data/initial_setup.sql
@@ -8,7 +8,7 @@ ON CONFLICT DO NOTHING;
 INSERT INTO
     users (name, email, password_hash, role_id)
 SELECT
-    'Eleazar Fig'
+    'Eleazar Fig',
     'eleazar.fig@example.com',
     '$2b$12$QPYXNL9a4VsBBw4j877gVOt5g1wfjBzA0xrDkDbJQScgQhQk31o76',
     role_id
