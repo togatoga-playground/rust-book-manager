44283e82d79d120dbee34c9dbe2b0c8704069580
togatoga <hitoshitogasaki@gmail.com>
2024-11-05 09:30:18 +0900

    Update

--- adapter/src/repository/checkout.rs
+++ adapter/src/repository/checkout.rs
@@ -84,8 +84,88 @@ impl CheckoutRepository for CheckoutRepositoryImpl {
     }
 
     async fn update_returned(&self, event: UpdateReturned) -> AppResult<()> {
+        let mut tx = self.db.begin().await?;
+        self.set_transaction_serializable(&mut tx).await?;
+        let res = sqlx::query_as!(
+            CheckoutStateRow,
+            r#"
+                SELECT
+                    b.book_id,
+                    c.checkout_id AS "checkout_id?: CheckoutId",
+                    c.user_id AS "user_id?: UserId"
+                FROM books as b
+                LEFT OUTER JOIN checkouts AS c USING(book_id)
+                WHERE b.book_id = $1
+            "#,
+            event.book_id as _
+        )
+        .fetch_optional(&mut *tx)
+        .await
+        .map_err(AppError::SpecificOperationError)?;
+
+        match res {
+            None => {
+                return Err(AppError::EntityNotFound(format!(
+                    "The book ({}) was not found.",
+                    event.book_id
+                )));
+            }
+            Some(CheckoutStateRow {
+                checkout_id: Some(c),
+                user_id: Some(u),
+                ..
+            }) if (c, u) != (event.checkout_id, event.returned_by) => {
+                return Err(AppError::UnprocessableEntity(format!(
+                    "The specified checkout (ID({}), User({}), Book({})) cannot be returned.",
+                    event.checkout_id, event.returned_by, event.book_id
+                )));
+            }
+            _ => {}
+        }
+
+        let res = sqlx::query!(
+            r#"
+                INSERT INTO returned_checkouts
+                (checkout_id, book_id, user_id, checked_out_at, returned_at)
+                SELECT checkout_id, book_id, user_id, checked_out_at, $1
+                FROM checkouts
+                WHERE checkout_id = $2;
+            "#,
+            event.returned_at,
+            event.checkout_id as _
+        )
+        .execute(&mut *tx)
+        .await
+        .map_err(AppError::SpecificOperationError)?;
+
+        if res.rows_affected() < 1 {
+            return Err(AppError::EntityNotFound(
+                "No returning record has been created".into(),
+            ));
+        }
+
+        let res = sqlx::query!(
+            r#"
+                DELETE FROM checkouts
+                WHERE checkout_id = $1;
+            "#,
+            event.checkout_id as _
+        )
+        .execute(&mut *tx)
+        .await
+        .map_err(AppError::SpecificOperationError)?;
+
+        if res.rows_affected() < 1 {
+            return Err(AppError::EntityNotFound(
+                "No checkout record has been deleted".into(),
+            ));
+        }
+
+        tx.commit().await.map_err(AppError::TransactionError)?;
+
         todo!()
     }
+
     async fn find_returned_all(&self) -> AppResult<Vec<Checkout>> {
         todo!()
     }
