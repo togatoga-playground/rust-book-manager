0f397258f0c0ec73382328b40547ba85051756f9
togatoga <hitoshitogasaki@gmail.com>
2024-11-10 17:27:49 +0900

    Update

--- api/tests/api/book.rs
+++ api/tests/api/book.rs
@@ -68,3 +68,45 @@ async fn show_book_list_with_query_200(
 
     Ok(())
 }
+
+#[rstest]
+#[case("/books?limit=-1")]
+#[case("/books?offset=aaa")]
+#[tokio::test]
+async fn show_book_list_with_query_400(
+    mut fixture: registry::MockAppRegistryExt,
+    #[case] path: &str,
+) -> anyhow::Result<()> {
+    let book_id = BookId::new();
+
+    fixture.expect_book_repository().returning(move || {
+        let mut mock = MockBookRepository::new();
+        mock.expect_find_all().returning(move |opt| {
+            let items = vec![Book {
+                id: book_id,
+                title: "RustによるWebアプリケーション開発".to_string(),
+                author: "Yuki Toyoda".to_string(),
+                isbn: "978-4-297-11101-4".to_string(),
+                description: "RustによるWebアプリケーション開発".to_string(),
+                owner: BookOwner {
+                    id: UserId::new(),
+                    name: "Yuki Toyoda".to_string(),
+                },
+                checkout: None,
+            }];
+            Ok(PaginatedList {
+                total: 1,
+                limit: opt.limit,
+                offset: opt.offset,
+                items,
+            })
+        });
+        Arc::new(mock)
+    });
+
+    let app = make_router(fixture);
+    let req = Request::get(&v1(path)).bearer().body(Body::empty())?;
+    let resp = app.oneshot(req).await?;
+    assert_eq!(resp.status(), axum::http::StatusCode::BAD_REQUEST);
+    Ok(())
+}
